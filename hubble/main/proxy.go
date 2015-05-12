5e62141a37f6604106cd76d1e13819c7309adb64
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-05-12 14:59:26 +0200

    Support proxy options

--- hubble/main/proxy.go
+++ hubble/main/proxy.go
@@ -4,10 +4,30 @@ import (
 	"hubble/proxy"
 	"net/http"
 	"log"
+	"fmt"
+	"flag"
 )
 
 
 func main() {
+	var listenAddr string
+	var help bool
+
+	flag.BoolVar(&help, "h", false, "Print this help screen")
+	flag.StringVar(&listenAddr, "listen", ":8080", "Listining address")
+	flag.Parse()
+
+	printHelp := func() {
+		fmt.Println("proxy [options]")
+		flag.PrintDefaults()
+	}
+
+	if help {
+		printHelp()
+		return
+	}
+
+	log.Println("Start listing on", listenAddr)
 	http.HandleFunc("/", proxy.ProxyHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
